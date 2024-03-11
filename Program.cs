e50de3d83d77a2dfb61e1a23c078e2b36bdc8840
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-11 08:49:46 +0600

    const double pi = 3.14159; added

--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@ namespace Beecrowd_Area_1012
         static void Main(string[] args)
         {
             string[] values = Console.ReadLine().Split();
+            const double pi = 3.14159;
 
             Console.WriteLine("TRIANGULO: " + ((double.Parse(values[0]) * double.Parse(values[2])) / 2).ToString("0.000"));
-            Console.WriteLine("CIRCULO: " + ((Math.PI * (double.Parse(values[2]) * double.Parse(values[2])))).ToString("0.000"));
+            Console.WriteLine("CIRCULO: " + ((pi * (double.Parse(values[2]) * double.Parse(values[2])))).ToString("0.000"));
             Console.WriteLine("TRAPEZIO: " + (((double.Parse(values[0]) + double.Parse(values[1])) * double.Parse(values[2])) / 2).ToString("0.000"));
             Console.WriteLine("QUADRADO: " + (double.Parse(values[1]) * double.Parse(values[1])).ToString("0.000"));
             Console.WriteLine("RETANGULO: " + ((double.Parse(values[0]) * double.Parse(values[1]))).ToString("0.000"));
